b04100487ff7054196f5876755ab93e6c0daddf7
NangialaiStoman <nstoman7@gmail.com>
2021-11-05 03:04:46 +0430

    Create contact and about pages

---
+++ pages/about.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>About me page</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/contact.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>contact page</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
