626a2dd1fea25a868cd88b7376ecc29ceb70ccb0
NangialaiStoman <nstoman7@gmail.com>
2021-11-05 18:05:13 +0430

    Add projects images

--- store/index.js
+++ store/index.js
@@ -1,5 +1,17 @@
+import { v4 as uuidv4 } from "uuid";
+
 export const state = () => ({
-  // Todo
+  projectsHeading: "Projects Portfolio",
+  projectsDescription: "Some of the projects I have successfully completed",
+  projectsData: [
+    {
+      id: 1,
+      title: "Google Health Platform",
+      category: "Web Application",
+      img: "images/web-project-2.jpg",
+      singleProjectData: [{}],
+    },
+  ],
 });
 
 export const getters = {
