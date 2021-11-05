f9860cb5bf7470f96a60d297947212ae14daf4f1
NangialaiStoman <nstoman7@gmail.com>
2021-11-05 17:09:35 +0430

    App transiations

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <AppHeader />
-    <Nuxt />
+    <transition name="fade" mode="out-in">
+      <Nuxt />
+    </transition>
     <AppFooter />
   </div>
 </template>
@@ -9,3 +11,38 @@
 <script>
 export default {};
 </script>
+
+<style>
+.fade-enter-active {
+  animation: coming 0.4s;
+  animation-delay: 0.2s;
+  opacity: 0;
+}
+
+.fade-leave-active {
+  animation: going 0.4s;
+}
+
+@keyframes going {
+  from {
+    transform: translateX(0);
+  }
+
+  to {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+}
+
+@keyframes coming {
+  from {
+    transform: translateX(-10px);
+    opacity: 0;
+  }
+
+  to {
+    transform: translateX(0px);
+    opacity: 1;
+  }
+}
+</style>
