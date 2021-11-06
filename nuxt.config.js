98bbbeeb1bf0b7cb4a63f2832e7d9d35ae6c8548
NangialaiStoman <nstoman7@gmail.com>
2021-11-06 13:21:48 +0430

    Use plugins

--- nuxt.config.js
+++ nuxt.config.js
@@ -24,7 +24,9 @@ export default {
   css: [],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [],
+  plugins: [
+    // "~/plugins/back-to-top.js"
+  ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
