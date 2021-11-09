d97a9617d13c49b3c35643b12108ec199f689573
NangialaiStoman <nstoman7@gmail.com>
2021-11-09 14:10:12 +0430

    Create about counter ui

---
+++ components/AboutCounter.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="mt-10 sm:mt-20 bg-primary-light dark:bg-ternary-dark shadow-sm">
+    <!-- About me counters start -->
+    <div
+      class="
+        container
+        mx-auto
+        py-20
+        block
+        sm:flex sm:justify-between sm:items-center
+      "
+    >
+      <!-- About me single counter start -->
+      <div class="mb-20 sm:mb-0">
+        <span
+          class="
+            text-4xl
+            font-bold
+            text-secondary-dark
+            dark:text-secondary-light
+            mb-2
+          "
+          >7+</span
+        >
+        <span class="block text-md text-ternary-dark dark:text-ternary-light"
+          >Years of experience</span
+        >
+      </div>
+      <!-- About me single counter end -->
+
+      <!-- About me single counter start -->
+      <div class="mb-20 sm:mb-0">
+        <span
+          class="
+            text-4xl
+            font-bold
+            text-secondary-dark
+            dark:text-secondary-light
+            mb-2
+          "
+          >2k</span
+        >
+        <span class="block text-md text-ternary-dark dark:text-ternary-light"
+          >Stars on GitHub</span
+        >
+      </div>
+      <!-- About me single counter end -->
+
+      <!-- About me single counter start -->
+      <div class="mb-20 sm:mb-0">
+        <span
+          text-4xl
+          font-bold
+          text-secondary-dark
+          dark:text-secondary-light
+          mb-2
+          >32</span
+        >
+        <span class="block text-md text-ternary-dark dark:text-ternary-light"
+          >Positive feedback</span
+        >
+      </div>
+      <!-- About me single counter end -->
+
+      <!-- About me single counter start -->
+      <div class="mb-20 sm:mb-0">
+        <span
+          class="
+            text-4xl
+            font-bold
+            text-secondary-dark
+            dark:text-secondary-light
+            mb-2
+          "
+          >77</span
+        >
+        <span class="block text-md text-ternary-dark dark:text-ternary-light"
+          >Projects completed</span
+        >
+      </div>
+      <!-- About me single counter end -->
+    </div>
+    <!-- About me counters end -->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped></style>
