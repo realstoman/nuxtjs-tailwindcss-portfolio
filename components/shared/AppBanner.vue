49d47fa2d80c86cc9e4e1f0cbf9e8a901d04ec09
realstoman <realstoman@gmail.com>
2022-01-24 11:28:00 +0430

    Banner fonts transition and svg illustrations

--- components/shared/AppBanner.vue
+++ components/shared/AppBanner.vue
@@ -6,19 +6,19 @@
       items-center
       sm:flex-row
       mt-12
-      md:mt-2
+      sm:mt-10
     "
   >
     <!-- Banner left contents -->
     <div class="w-full md:w-1/3 text-left">
       <h1
         class="
+          font-general-semibold
           text-3xl
-          md:text-4xl
-          xl:text-5xl
+          md:text-3xl
+          xl:text-4xl
           text-center
           sm:text-left
-          font-semibold
           text-ternary-dark
           dark:text-primary-light
           uppercase
@@ -28,13 +28,13 @@
       </h1>
       <p
         class="
-          mt-4
-          text-2xl
-          md:text-3xl
-          xl:text-4xl
+          font-general-medium
+          mt-2
+          text-lg
+          sm:text-xl
+          xl:text-2xl
           text-center
           sm:text-left
-          font-semibold
           leading-none
           text-gray-400
         "
@@ -66,22 +66,31 @@
             hover:bg-indigo-500
             text-gray-500
             hover:text-white
+            duration-500
           "
           aria-label="Download Resume"
         >
           <i
             data-feather="arrow-down-circle"
-            class="ml-0 sm:ml-1 mr-2 sm:mr-3 w-5 sm:w-6"
+            class="ml-0 sm:ml-1 mr-2 sm:mr-3 w-5 sm:w-6 duration-100"
           ></i>
-          <span class="text-sm sm:text-lg">Download CV</span></a
+          <span class="text-sm sm:text-lg font-general-medium duration-100"
+            >Download CV</span
+          ></a
         >
       </div>
     </div>
 
     <!-- Banner right illustration -->
-    <div class="w-full md:w-2/3 text-right float-right mt-4 sm:mt-0">
-      <DeveloperDarkIllustration v-if="this.$colorMode.value == 'dark'" />
-      <DeveloperLightIllustration v-else />
+    <div class="w-full md:w-2/3 text-right float-right">
+      <!-- <DeveloperDarkIllustration v-if="this.$colorMode.value == 'dark'" />
+      <DeveloperLightIllustration v-else /> -->
+      <img
+        v-if="this.$colorMode.value == 'dark'"
+        src="~/static/developer-dark.svg"
+        alt="Developer Dark"
+      />
+      <img v-else src="~/static/developer.svg" alt="Developer Light" />
     </div>
   </section>
 </template>
